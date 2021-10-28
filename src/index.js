b3cc780aca3cca42dccc00d53261c2d94ff4e867
IanHancock36 <71861289+IanHancock36@users.noreply.github.com>
2021-10-27 21:46:01 -0500

    fetch api json added json response and console log to data.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@
 fetch("https://jsonplaceholder.typicode.com/todos/")
   .then((response) => {
     console.log(response, "resolved");
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
   })
   .catch((error) => {
     console.log("rejected", error);
