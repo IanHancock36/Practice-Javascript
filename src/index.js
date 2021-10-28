1b74442e2da2d39ada690224d76f7bbb714998ca
IanHancock36 <71861289+IanHancock36@users.noreply.github.com>
2021-10-27 22:08:01 -0500

    async error error log throw new errors

--- src/index.js
+++ src/index.js
@@ -18,8 +18,14 @@
 // await chains the promises.
 
 const getTodos = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos/");
+  const response = await fetch("https://jsonplaceholdefr.typicode.com/todos/");
   const data = await response.json();
+  if (response.status !== 200) {
+    throw new Error("cannot fetch data");
+  }
   return data;
 };
-getTodos(); // this is a promise.
+getTodos()
+  .then((data) => console.log(data, "resloved"))
+  .catch((error) => console.log(error, "ERROR"));
+//returns a promise
