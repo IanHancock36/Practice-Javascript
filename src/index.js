58123da5b3801e51382f0215cf7335d07196e9bd
IanHancock36 <71861289+IanHancock36@users.noreply.github.com>
2021-10-27 21:40:31 -0500

    fetch fetch api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import "./styles.css";
+// fetch api
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+fetch("https://jsonplaceholder.typicode.com/todos/")
+  .then((response) => {
+    console.log(response, "resolved");
+  })
+  .catch((error) => {
+    console.log("rejected", error);
+  });
