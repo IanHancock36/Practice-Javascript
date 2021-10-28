1e747f33d9facd3a1169b20cdc65c82b36b3a9a7
IanHancock36 <71861289+IanHancock36@users.noreply.github.com>
2021-10-27 21:56:43 -0500

    async async and await

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 // fetch api
 
-fetch("https://jsonplaceholder.typicode.com/todos/")
-  .then((response) => {
-    console.log(response, "resolved");
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => {
-    console.log("rejected", error);
-  });
+// fetch("https://jsonplaceholder.typicode.com/todos/")
+//   .then((response) => {
+//     console.log(response, "resolved");
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     console.log("rejected", error);
+//   });
+
+//async and await
+
+// async function always returns a promise...
+// await chains the promises.
+
+const getTodos = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/");
+  const data = await response.json();
+  return data;
+};
+getTodos(); // this is a promise.
